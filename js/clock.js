752cb85e738832693ad6161bc38f9430054f1f1e
SiMONA22 <100865207+simonasablinaite@users.noreply.github.com>
2022-11-03 16:19:13 +0200

    optimisation

--- js/clock.js
+++ js/clock.js
@@ -1,42 +1,38 @@
-//Data iki kurios skaiciuojamas laikas (su valandomis)
-const countDownDate = new Date("December 23, 2022 12:00:00").getTime();
+//Data iki kurios skaiciuojamas laikas
+const countDownDate = new Date("Dec 23, 2022 12:00:00").getTime();
 
 //Skaiciavimo atnaujinimas kas 1s
 const x = setInterval(function () {
-  //pirmiausiai gaunama šiandienos data ir laikas
+  //šiandienos data ir laikas
   const now = new Date().getTime();
 
-  //Laikotarpis nuo šiandienos iki deadlin'o
-  const distance = countDownDate - now;
+  //Laikas nuo dabar iki deadlin'o
+  const diff = countDownDate - now;
+
+  // Sukuriamas sekundės kintamasis
+  const second = 1000 * 60;
 
   //Apskaiciuojamas laikas dienomis, valandomis, minutemis ir sekundemis
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const days = Math.floor(diff / (second * 60 * 24));
 
-  const hours = Math.floor(
-    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  const formattedHours = ("0" + hours).slice(-2);
+  const hours = (
+    "0" + Math.floor((diff % (second * 60 * 24)) / (second * 60))
+  ).slice(-2);
 
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const formatteMinutes = ("0" + minutes).slice(-2);
+  const minutes = ("0" + Math.floor((diff % (second * 60)) / second)).slice(-2);
 
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  const formatteSeconds = ("0" + seconds).slice(-2);
+  const seconds = ("0" + Math.floor((diff % second) / 1000)).slice(-2);
 
   //Isvedamas elementas
-  document.getElementById("time").innerHTML =
-    days +
-    " " +
-    formattedHours +
-    " " +
-    formatteMinutes +
-    " " +
-    formatteSeconds +
-    " ";
+  document.getElementById("time").textContent = `${days} 
+    ${hours} 
+    ${minutes} 
+    ${seconds}
+    `;
 
   //Baigus skaiciavima - nurodomas tekstas
-  if (distance < 0) {
+  if (diff < 0) {
     clearInterval(x);
-    document.getElementById("time").innerHTML = "LAIKAS BAIGĖSI! :(";
+    document.getElementById("time").textContent = "LAIKAS BAIGĖSI! :(";
   }
-}, 1000);
+});
