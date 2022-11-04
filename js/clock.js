f1182dd4bfc10b04ca0c1317f669810508542e97
SiMONA22 <100865207+simonasablinaite@users.noreply.github.com>
2022-11-04 17:54:25 +0200

    Code optimization

--- js/clock.js
+++ js/clock.js
@@ -1,16 +1,16 @@
 const countDownDate = new Date("Dec 23, 2022 12:00:00").getTime();
-
-const clockDOM = document.getElementById("time");
-
 const format = (n) => (n < 10 ? "0" + n : n);
 
 const x = setInterval(function () {
+  const clockDOM = document.getElementById("time");
+
   const now = new Date().getTime();
-  const diff = Math.floor((countDownDate - now) / 1000);
+
+  let diff = Math.floor((countDownDate - now) / 1000);
 
   if (diff <= 0) {
     clearInterval(x);
-    clockDOM.textContent = "LAIKAS BAIGĖSI! :(";
+    clockDOM.innerText = "LAIKAS BAIGĖSI! :(";
   } else {
     const seconds = diff % 60;
     diff = (diff - seconds) / 60;
@@ -21,11 +21,10 @@ const x = setInterval(function () {
     const hours = diff % 24;
     diff = (diff - hours) / 24;
 
-    const days = diff / 24;
-
     // const days = diff / 24;
-    clockDOM.textContent = `${diff} ${days} ${format(hours)} ${format(
-      minutes
-    )} ${format(seconds)}`;
+
+    clockDOM.innerText = `${diff} ${format(hours)} ${format(minutes)} ${format(
+      seconds
+    )}`;
   }
 });
